5189792df9e2f2d46f4cc68bb7c4bd14bebb3f25
BrickEater <codyratcliffep@gmail.com>
2024-04-25 12:34:10 -0600

    beginnings of the interface

--- main.c
+++ main.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
+enum options_e { HELP = 'h', VERSION = 'v', TCP_SOCKET = 's' };
 
 int main(int argc, char *argv[]) {
-  if (argc == 1) {
-    printf("the help menu should go here");
+
+  if (argc > 1 && argv[1][0] == '-') {
+    switch (argv[1][1]) {
+    case HELP:
+      printf("This should be a function that prints the help menu\n");
+      break;
+    case VERSION:
+      printf("This should be a function that prints the version\n");
+      break;
+    case TCP_SOCKET:
+      printf("This should be a function that creates a TCP socket\n");
+      break;
+    }
+  } else {
+    printf("No or incorrect command line argument provided.\n");
   }
+
   return 0;
 }
